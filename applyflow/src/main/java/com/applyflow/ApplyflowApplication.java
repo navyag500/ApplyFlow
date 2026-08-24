172d231818d6fff71610088e9fa14ecffc13a4d9
Navya Gupta <navyag500@gmail.com>
2026-08-24 12:07:32 +0530

    Initial Spring Boot project setup

---
+++ applyflow/src/main/java/com/applyflow/ApplyflowApplication.java
@@ -0,0 +1,13 @@
+package com.applyflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplyflowApplication.class, args);
+	}
+
+}
---
+++ applyflow/src/test/java/com/applyflow/ApplyflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applyflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplyflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
